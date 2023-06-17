703f85a36cbf3677f0ccc2322d400555f7e9e828
Ewald Horn <ewaldhorn@gmail.com>
2023-06-17 14:08:57 +0200

    Use pointers to change values

--- fcc/pointers/changeval.c
+++ fcc/pointers/changeval.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-void change_value(int nb);
+void change_value(int *num);
 
 int main()
 {
     int nb = 42;
-    change_value(nb);
+    
+    change_value(&nb);
     printf("The value is %d.\n", nb);
+
     return 0;
 }
 
-void change_value(int nb) {
-    nb = 1337;
+void change_value(int *num) {
+    *num = 1337;
 }
