80b26fdc6704c13591dcd1a7296e8c7b1cc842ee
Ewald Horn <ewaldhorn@gmail.com>
2023-06-17 15:12:03 +0200

    Pointer declarations

---
+++ fcc/pointers/declarations.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, *pn, **ppn, ***pppn;
+
+    n = 42;        // value
+    pn = &n;       // pointer to value
+    ppn = &pn;     // pointer to pointer of value
+    pppn = &ppn;   // pointer to pointer to pointer of value
+
+    printf("%p == %p\n", *pppn, ppn);
+
+    printf("The value of n    is %d.\n", n);
+    printf("The value of pn   is %d.\n", *pn);
+    printf("The value of ppn  is %d.\n", **ppn);
+    printf("The value of pppn is %d.\n", ***pppn);
+
+    return 0;
+}
