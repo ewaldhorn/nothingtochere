a4000c2a4dd8f01e782b56a5cbcfdf2093dcc044
Ewald Horn <ewaldhorn@gmail.com>
2023-06-18 11:33:00 +0200

    SoloLearn C unions

---
+++ lessons/sololearncint/unions.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+union val
+{
+    int int_num;
+    float fl_num;
+    char str[20];
+};
+
+int main()
+{
+    union val u1;
+    union val u2;
+    u2 = u1;
+
+    union val test;
+
+    test.int_num = 123;
+    test.fl_num = 98.76;
+    strcpy(test.str, "hello");
+
+    printf("%d\n", test.int_num);
+    printf("%f\n", test.fl_num);
+    printf("%s\n", test.str);
+
+    return 0;
+}
