a1723828de07603894fe127444bc3aa2c002c572
Ewald Horn <ewaldhorn@gmail.com>
2023-06-19 17:58:29 +0200

    Pointer size remains the same.

---
+++ fcc/pointers/sizes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct s_big
+{
+    int v[1000000];
+} t_big;
+
+int main()
+{
+    char c;
+    short sh;
+    int i;
+    double d;
+    t_big big;
+
+    printf("%zu ptr char\n"
+           "%zu ptr short\n"
+           "%zu ptr int\n"
+           "%zu ptr double\n"
+           "%zu ptr big struct\n",
+           sizeof(&c),
+           sizeof(&sh),
+           sizeof(&i),
+           sizeof(&d),
+           sizeof(&big));
+
+    return 0;
+}
