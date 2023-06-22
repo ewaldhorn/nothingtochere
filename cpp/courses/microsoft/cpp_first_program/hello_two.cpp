135821c79562f390d1a026c003ad9520fe20bae7
Ewald Horn <ewaldhorn@gmail.com>
2023-06-22 11:24:00 +0200

    Checking Microsoft CPP tutorial out

---
+++ cpp/courses/microsoft/cpp_first_program/hello_two.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout << "Hello World!\n" << endl;
+    cout << "Congratulations!";
+    cout << " ";
+    cout << "You wrote your first lines of code in C++.\n";
+    return 0;
+}
