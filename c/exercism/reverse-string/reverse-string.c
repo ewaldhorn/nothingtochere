4ec93ebf5fa1b0aac14ce0ac02b9054f2bea5b7a
Ewald Horn <ewaldhorn@gmail.com>
2023-07-02 12:05:24 +0200

    Reverse string in C

---
+++ c/exercism/reverse-string/reverse-string.c
@@ -0,0 +1,29 @@
+#include "reverse-string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char *tmp = "loop";
+    printf("%s reversed is %s\n", tmp, reverse(tmp));
+
+    return 0;
+}
+
+char *reverse(const char *value)
+{
+	int	length = strlen(value);
+	char *result;
+    result = malloc(sizeof(char) * (length + 1));
+
+	int i = 0;
+	while (i < length)
+	{
+		result[i] = value[length - 1 - i];
+		i++;
+	}
+
+	result[i] = '\0'; // don't forget the string terminator!
+	
+    return result;
+}
