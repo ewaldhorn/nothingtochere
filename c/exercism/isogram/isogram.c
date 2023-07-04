3a8663c0243fa020d74819691551b363bb4b8382
Ewald Horn <ewaldhorn@gmail.com>
2023-07-04 13:09:54 +0200

    Exercism Isogram in C

--- c/exercism/isogram/isogram.c
+++ c/exercism/isogram/isogram.c
@@ -3,75 +3,29 @@
 #include "string.h"
 #include "ctype.h"
 
-int main() 
-{
-    char phrase[] = "loOP";
-    printf("%12s is an isogram %d.\n", phrase, is_isogram(phrase)); // no
-
-    char phraseTwo[] = "lumBErjack";
-    printf("%12s is an isogram %d.\n", phraseTwo, is_isogram(phraseTwo)); // yes
-
-    char phraseThree[] = "raCEcar";
-    printf("%12s is an isogram %d.\n", phraseThree, is_isogram(phraseThree)); // no
-
-    char phraseFour[] = "Six-Year-Old";
-    printf("%12s is an isogram %d.\n", phraseFour, is_isogram(phraseFour)); // yes
-
-    return 0;
-}
-
 bool is_isogram(const char phrase[])
 {
-    if (strlen(phrase) == 0)
+
+    if (phrase == NULL)
     {
         return false;
     }
 
-    // make a copy of the phrase
-    char workPhrase[strlen(phrase)+1];
-    strcpy(workPhrase, phrase);
-
-    // now make it lowercase, to make things easier
-    for (long unsigned int i = 0; i < strlen(workPhrase); i++) {
-        workPhrase[i] = tolower(workPhrase[i]);
+    // now compare as lowercase, to make things easier
+    for (long unsigned int i = 0; i < strlen(phrase); i++)
+    {
+        char tmp = tolower(phrase[i]);
+        if (tmp != '-' && tmp != ' ')
+        {
+            for (long unsigned int j = i + 1; j < strlen(phrase); j++)
+            {
+                if (tmp == tolower(phrase[j]))
+                {
+                    return false;
+                }
+            }
+        }
     }
 
-    bool state = true;
-
-    return state;
+    return true;
 }
-
-// bool is_isogram(const char phrase[])
-// {
-//     if (strlen(phrase) == 0)
-//     {
-//         return false;
-//     }
-
-//     // make a copy of the phrase
-//     char workPhrase[strlen(phrase)+1];
-//     strcpy(workPhrase, phrase);
-
-//     // now make it lowercase, to make things easier
-//     for (long unsigned int i = 0; i < strlen(workPhrase); i++) {
-//         workPhrase[i] = tolower(workPhrase[i]);
-//     }
-
-//     bool state = true;
-
-//     for (long unsigned int i = 0; i < strlen(workPhrase); i++) {
-//         if (workPhrase[i] != '-' && workPhrase[i] != ' ')
-//         {
-//             // we don't have a hyphen or a space
-//             for (long unsigned int j = i+1; j < strlen(workPhrase); j++) {
-//                 if (workPhrase[i] == workPhrase[j])
-//                 {
-//                     state = false;
-//                     break;
-//                 }
-//             }
-//         }
-//     }
-
-//     return state;
-// }
