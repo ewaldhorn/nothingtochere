01386e21746373bbe058d234b915e9c4f28b03b8
Ewald Horn <ewaldhorn@gmail.com>
2024-02-03 15:20:02 +0200

    C Pointers

--- c/learning/singlefiles/pointers.c
+++ c/learning/singlefiles/pointers.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+void pointers() {
+    int count = 100, score = 25;
+    int *pCount = &count;
+    count = *pCount + 1;
+}
+
 void update(int *a,int *b) {
     int sum = *a + *b;
     int dif = *a - *b;
@@ -12,6 +18,16 @@ void update(int *a,int *b) {
     } 
 }
 
+void swap(int *left, int *right) {
+    int tmp = *left;
+    *left = *right;
+    *right = tmp;
+}
+
+int alternativeStringLength(char *string) {
+    return strlen(string);
+}
+
 int main() {
     int a, b;
     int *pa = &a, *pb = &b;
@@ -19,6 +35,13 @@ int main() {
     scanf("%d %d", &a, &b);
     update(pa, pb);
     printf("%d\n%d", a, b);
+
+    char* yolo = "YOLO!";
+    int (*aFuncPtr)(char *);
+    aFuncPtr = alternativeStringLength;
+
+    int length = (*aFuncPtr)(yolo);
+    printf("The string length is %d\n", length);
 
     return 0;
 }
