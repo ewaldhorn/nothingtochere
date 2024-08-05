7a4eb0d38b236eb827145f2016909ab4be703796
Ewald Horn <ewaldhorn@gmail.com>
2024-08-05 15:37:29 +0200

    Amend to use a header file as well

--- c/experiments/stacks/simple_stack/csrc/simple_stack.c
+++ c/experiments/stacks/simple_stack/csrc/simple_stack.c
@@ -1,11 +1,4 @@
-// #include <stdio.h>
-#include <limits.h>
-#include <stdbool.h>
-
-// -------------------------------------------------------------------- defines
-#define MAX_STACK_LENGTH 5
-#define EMPTY (-1)
-#define STACK_EMPTY INT_MIN
+#include "simple_stack.h"
 
 // -------------------------------------------------------------------- globals
 int theStack[MAX_STACK_LENGTH];
---
+++ c/experiments/stacks/simple_stack/csrc/simple_stack.h
@@ -0,0 +1,8 @@
+// #include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
+
+// -------------------------------------------------------------------- defines
+#define MAX_STACK_LENGTH 5
+#define EMPTY (-1)
+#define STACK_EMPTY INT_MIN
