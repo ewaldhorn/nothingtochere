18b856b72849cc111fd53bb2945648f1bb98fbec
Ewald Horn <ewaldhorn@gmail.com>
2024-08-05 13:04:57 +0200

    Explaining how a stack works

---
+++ c/experiments/stacks/simple_stack/simple_stack.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
+
+// -------------------------------------------------------------------- defines
+#define MAX_STACK_LENGTH 5
+#define EMPTY (-1)
+#define STACK_EMPTY INT_MIN
+
+// -------------------------------------------------------------------- globals
+int theStack[MAX_STACK_LENGTH];
+int top = EMPTY;
+
+// ----------------------------------------------------------------------- push
+bool push(int val) {
+  if (top >= MAX_STACK_LENGTH-1) return false; // abort if stack is full
+
+  top++;
+  theStack[top] = val;
+  return true;
+}
+
+// ------------------------------------------------------------------------ pop
+int pop() {
+  if (top == EMPTY) return STACK_EMPTY;
+
+  int result = theStack[top];
+  top--;
+
+  return result;
+}
+
+// ======================================================================= main
+int main() {
+  return 0;
+}
