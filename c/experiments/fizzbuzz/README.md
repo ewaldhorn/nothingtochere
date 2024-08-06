fbbc3d28e57c49362bd0951ce352ea4036c6a28c
Ewald Horn <ewaldhorn@gmail.com>
2024-08-06 14:48:51 +0200

    Adding another C and Zig interaction example

---
+++ c/experiments/fizzbuzz/README.md
@@ -0,0 +1,5 @@
+# Use Zig to test C FizzBuzz solution
+
+Use Zig to create tests for a FizzBuzz solution written in C. Shows Zig calling C code and Zig tests testing C code. Also shows how to receive strings from C.
+
+A Task file (<https://taskfile.dev/>) is included for convenience.
---
+++ c/experiments/fizzbuzz/build.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "fizzbuzz",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkLibC(); // link against LibC
+    exe.addIncludePath(b.path("csrc")); // include C sources
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe_unit_tests.linkLibC(); // link against LibC
+    exe_unit_tests.addIncludePath(b.path("csrc")); // include C sources
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
