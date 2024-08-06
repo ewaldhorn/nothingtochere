4cb2b977ce969548c4377b859ca7e9a6273e77e8
Ewald Horn <ewaldhorn@gmail.com>
2024-08-06 15:35:43 +0200

    More Zig calling into C experiments

--- c/experiments/fizzbuzz/csrc/fizz_buzzer.c
+++ c/experiments/fizzbuzz/csrc/fizz_buzzer.c
@@ -1,1 +1,24 @@
 #include "fizz_buzzer.h"
+
+// --------------------------------------------------------------------- buzzer
+const char* buzzer(const int n) {
+  char *result;
+
+  if (n % 15 == 0){
+    result = "FizzBuzz";
+  } else if (n % 3 == 0) {
+    result = "Fizz";
+  } else if (n % 5 == 0) {
+    result = "Buzz";
+  } else {
+    asprintf(&result, "%d", n);
+  }
+
+  return result;
+}
+
+// -------------------------------------------------------------------- version
+/// returns the version of the app as a string
+const char* version() {
+  return "0.0.1";
+}
--- c/experiments/fizzbuzz/src/main.zig
+++ c/experiments/fizzbuzz/src/main.zig
@@ -4,10 +4,22 @@ const fizzBuzzer = @cImport({
     @cInclude("fizz_buzzer.c");
 });
 
+const cStrings = @cImport({
+    @cInclude("string.h");
+});
+
 pub fn main() void {}
 
 // ====================================================================== TESTS
 const expect = std.testing.expect;
+
+// ----------------------------------------------------------------------------
+test "zig calling C to get length of string" {
+    const input = "One two three!";
+    const result = cStrings.strlen(input);
+
+    try expect(result == input.len);
+}
 
 // ----------------------------------------------------------------------------
 test "1 => 1" {
