5efd85b98a5c7c6229bd6cdcf0bc1da0638bd9ab
Ewald Horn <ewaldhorn@gmail.com>
2024-08-11 15:31:45 +0200

    WIP

--- c/codingame/logic_gates/main.c
+++ c/codingame/logic_gates/main.c
@@ -61,32 +61,67 @@ char *NXOR(char *left, char *right) {
   }
 }
 
+// --------------------------------------------------------------------- STRUCT
+struct InputStruct {
+  char *name;
+  char *signal;
+};
+
+struct InstructionsStruct {
+  char *name;
+  char *gate;
+  char *left;
+  char *right;
+};
+
 // ======================================================================= MAIN
 int main() {
   int inputSignals, outputSignals;
   scanf("%d", &inputSignals);
   scanf("%d", &outputSignals);
 
+  struct InputStruct inputs[10];
+  struct InstructionsStruct instructions[10];
+
+  // read all the input signals
   for (int i = 0; i < inputSignals; i++) {
-    char input_name[9];
-    char input_signal[65];
+    char input_name[2];
+    char input_signal[100];
     scanf("%s%s", input_name, input_signal);
+    fprintf(stderr, "Read: %s %s\n", input_name, input_signal);
+    inputs[i].name = input_name;
+    inputs[i].signal = input_signal;
   }
 
+  // now read all the output signals
   for (int i = 0; i < outputSignals; i++) {
     char output_name[9];
     char type[9];
     char input_name_1[9];
     char input_name_2[9];
     scanf("%s%s%s%s", output_name, type, input_name_1, input_name_2);
+    fprintf(stderr, "Read %s %s %s %s\n", output_name, type, input_name_1,
+            input_name_2);
+    instructions[i].name = output_name;
+    instructions[i].gate = type;
+    instructions[i].left = input_name_1;
+    instructions[i].right = input_name_2;
   }
 
   for (int i = 0; i < outputSignals; i++) {
+    fprintf(stderr, "%s %s %s %s\n", instructions[i].name, instructions[i].gate,
+            instructions[i].left, instructions[i].right);
+  }
 
-    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
-    // To debug: fprintf(stderr, "Debug messages...\n");
+  // now process it
+  for (int i = 0; i < outputSignals; i++) {
+    printf("%s ", instructions[i].name);
 
-    printf("output name and signal\n");
+    int len = strlen(instructions[i].left);
+    for (int pos = 0; pos < len; pos++) {
+    }
+
+    printf("\n");
   }
 
   return 0;
