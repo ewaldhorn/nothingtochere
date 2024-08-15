67dbc003765e3f9e758500c9e603146dc5225381
Ewald Horn <ewaldhorn@gmail.com>
2024-08-15 12:47:13 +0200

    More pointers

--- c/learning/zerotohero/pointers/pointers.c
+++ c/learning/zerotohero/pointers/pointers.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+// --------------------------------------------------------------------- struct
+struct person_t {
+  int id;
+  int paygrade;
+  char *name;
+};
 
 // ------------------------------------------------------------- simple_pointer
 void simple_pointer(void) {
@@ -26,12 +32,34 @@ void simple_pointer(void) {
   printf("The value of the pointer is: %d\n", *pointerToNumber);
 }
 
+// ---------------------------------------------------------- upgrade_pay_grade
+void upgrade_pay_grade(struct person_t *person, int increase) {
+  person->paygrade += increase;
+}
+
 // -------------------------------------------------------------- more_pointers
-void more_pointers(void) {}
+void more_pointers(void) {
+  struct person_t operator;
+
+  operator.id = 10;
+  operator.paygrade = 1;
+  operator.name = "Kaspaas";
+
+  puts("\nOperater details:");
+  printf("ID   : %d\nName : %s\nGrade: %d\n", operator.id, operator.name,
+                                                           operator.paygrade);
+
+  upgrade_pay_grade(&operator, 2);
+
+  puts("\nOperater updated details:");
+  printf("ID   : %d\nName : %s\nGrade: %d\n", operator.id, operator.name,
+                                                           operator.paygrade);
+}
 
 // ======================================================================= main
 int main() {
   simple_pointer();
+  more_pointers();
 
   return EXIT_SUCCESS;
 }
