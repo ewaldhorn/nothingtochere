56da2b341637b4fcd282e82b4d747399853af4db
Ewald Horn <ewaldhorn@gmail.com>
2024-10-01 21:05:05 +0200

    Basics working

--- c/experiments/sudoku/main.c
+++ c/experiments/sudoku/main.c
@@ -4,6 +4,7 @@
 
 #include "tools.h"
 
+int board[9][9];
 // ----------------------------------------------------------------------------
 // Ensure inputs are valid
 char *checkInputs(int argc, char *argv[]) {
@@ -29,8 +30,8 @@ char *checkInputs(int argc, char *argv[]) {
   return argv[1];
 }
 // ----------------------------------------------------------------------------
-// Populate board from input, spaces become 0, others numbers
-void populateBoard(int board[][9], char *inputString) {
+// Populate board from input, spaces (.) become 0, others numbers
+void populateBoard(char *inputString) {
   for (int col = 0; col < 9; col++) {
     for (int row = 0; row < 9; row++) {
       char tmp = inputString[col * 9 + row];
@@ -44,7 +45,7 @@ void populateBoard(int board[][9], char *inputString) {
 }
 // ----------------------------------------------------------------------------
 // Display the board
-void printBoard(int board[][9]) {
+void printBoard() {
   for (int col = 0; col < 9; col++) {
     for (int row = 0; row < 9; row++) {
       printf("%d ", board[col][row]);
@@ -52,10 +53,71 @@ void printBoard(int board[][9]) {
     printf("\n");
   }
 }
+
 // ----------------------------------------------------------------------------
+int isValid(int row, int col, int num) {
+  // check the line
+  for (int i = 0; i < 9; i++) {
+    if (board[row][i] == num) {
+      return FALSE;
+    }
+  }
+
+  // Check the column
+  for (int i = 0; i < 9; i++) {
+    if (board[i][col] == num) {
+      return FALSE;
+    }
+  }
+
+  // Check the box
+  int startRow = row - row % 3;
+  int startCol = col - col % 3;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (board[i + startRow][j + startCol] == num) {
+        return FALSE;
+      }
+    }
+  }
+
+  return TRUE;
+}
+
 // ----------------------------------------------------------------------------
+int solveSudoku() {
+  for (int row = 0; row < 9; row++) {
+    for (int col = 0; col < 9; col++) {
+      if (board[row][col] == 0) {
+        for (int num = 1; num <= 9; num++) {
+          if (isValid(row, col, num) == TRUE) {
+            board[row][col] = num;
+            if (solveSudoku() == TRUE) {
+              return TRUE;
+            } else {
+              // that didn't work, reset
+              board[row][col] = 0;
+            }
+          }
+        }
+        return FALSE;
+      }
+    }
+  }
+
+  return TRUE;
+}
+
 // ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
+void printBoardAsString() {
+  for (int row = 0; row < 9; row++) {
+    for (int col = 0; col < 9; col++) {
+      printf("%d", board[row][col]);
+    }
+  }
+  printf("\n");
+}
+
 // ----------------------------------------------------------------------------
 int main(int argc, char *argv[]) {
 
@@ -68,13 +130,22 @@ int main(int argc, char *argv[]) {
   printf("The input puzzle:\n");
   printAsPuzzle(input);
 
+  // create the board
+  populateBoard(input);
+  printf("Becomes a board:\n");
+  printBoard();
+  printf("\n");
+
   printf("=======================\n");
   printf("Calculation solution...\n");
+  solveSudoku();
+  printBoard();
 
-  // create the board
-  int board[9][9];
-  populateBoard(board, input);
-  printBoard(board);
+  printf("\n\n");
+  printBoardAsString();
+  char *expected = "53467891267219534819834256785976142342685379171392485696153"
+                   "7284287419635345286179";
+  printf("%s\n", expected);
 
   return EXIT_SUCCESS;
 }
--- c/experiments/sudoku/tools.h
+++ c/experiments/sudoku/tools.h
@@ -1,6 +1,9 @@
 #ifndef __TOOLS_H_
 #define __TOOLS_H_
 
+#define TRUE 1
+#define FALSE 0
+
 void printHelp(char *extraMessage);
 int containsInvalidCharacters(char *inputString);
 void printAsPuzzle(char *inputString);
