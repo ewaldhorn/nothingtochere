2ef72e63408ee0798ecb2c15b88b2bac7f57122a
Ewald Horn <ewaldhorn@gmail.com>
2025-10-15 22:35:23 +0200

    Housekeeping

--- c/wasm/no_std/just_cube/debugbuild.sh
+++
@@ -1,9 +0,0 @@
-clang \
-    --target=wasm32 \
-    -nostdlib \
-    -Wl,--no-entry \
-    -Wl,--export=cube \
-    -Wl,--export=square \
-    -Wl,--allow-undefined \
-    -o ./docs/math.wasm \
-    ./src/math.c
